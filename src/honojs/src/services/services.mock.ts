6a811d523a1f398404d67bd2a0d18e92474d9c0c
TheSmartMonkey <vandellelaurent@yahoo.fr>
2026-01-15 22:50:33 +0100

    add honojs jest config

---
+++ src/honojs/src/services/services.mock.ts
@@ -0,0 +1,29 @@
+import type { MockProxy } from 'jest-mock-extended';
+import { mock } from 'jest-mock-extended';
+import type { DeepPartial, DeepRequired } from 'ts-essentials';
+import { ThrowableWeapon } from './throwable-weapon';
+import { Warrior } from './warrior';
+import { Weapon } from './weapon';
+
+export function createMock<T>(mockImplementation: DeepRequired<T>): MockProxy<T> {
+  return mock<T>(mockImplementation as DeepPartial<T>);
+}
+
+export function mockWeapon(): MockProxy<Weapon> {
+  return createMock<Weapon>({
+    hit: jest.fn().mockImplementation(() => 'fake hit !'),
+  });
+}
+
+export function mockThrowableWeapon(): MockProxy<ThrowableWeapon> {
+  return createMock<ThrowableWeapon>({
+    throw: jest.fn().mockImplementation(() => 'fake throw !'),
+  });
+}
+
+export function mockWarrior(): MockProxy<Warrior> {
+  return createMock<Warrior>({
+    fight: jest.fn().mockImplementation(() => 'fake fight !'),
+    sneak: jest.fn().mockImplementation(() => 'fake sneak !'),
+  });
+}
